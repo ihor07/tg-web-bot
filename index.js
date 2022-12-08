a2d391930ae2a1a045a10ea428c8aacdae7ff898
Ihor <ngorekk@gmail.com>
2022-12-08 23:16:03 +0100

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+const token = '5823526718:AAESdvdx3ik1qMlRuv21Q5NWW1easW0BrRc'
+
+const webAppUrl = 'https://google.com'
+
+const bot = new TelegramBot(token, { polling: true })
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id
+  const text = msg.text
+
+  if (text === '/start') {
+    await bot.sendMessage(chatId, 'User Form', {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Make Order', web_app: { url: webAppUrl } }],
+        ],
+      },
+    })
+  }
+
+  bot.sendMessage(chatId, 'Received your message')
+})
