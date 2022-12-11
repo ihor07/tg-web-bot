3ae6a42f4c312708eb586b7475b6a771708b46a6
Ihor <ngorekk@gmail.com>
2022-12-11 14:56:31 +0100

    upd form

--- src/components/Forms/Forms.jsx
+++ src/components/Forms/Forms.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import './Forms.css'
 
 const Forms = () => {
-  return <div>Forms</div>
+  return (
+    <div className={'forms'}>
+      <h3>Input your info</h3>
+      <input className={'input'} type="text" placeholder={'Country'} />
+      <input className={'input'} type="text" placeholder={'Street'} />
+      <select className={'select'}>
+        <option value={'physic'}>physical</option>
+        <option value={'legal'}>legal</option>
+      </select>
+    </div>
+  )
 }
 
 export default Forms
