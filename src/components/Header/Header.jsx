75e2d630d657bbd5cffb08727a19fd6b263eb1b6
Ihor <ngorekk@gmail.com>
2022-12-11 12:57:11 +0100

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import { on } from 'nodemon'
+import React from 'react'
 import Button from '../Button/Button'
 import { useTg } from '../hooks/useTg'
-import React from 'react'
 
 const Header = () => {
   const { user, onClose } = useTg()
+
   return (
     <div className={'header'}>
       <Button onClick={onClose}>Close</Button>
