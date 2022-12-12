260b56860f9e9a9b74d2e25598dd21ae1976b819
Ihor <ngorekk@gmail.com>
2022-12-12 14:44:38 +0100

    useEffect new2

--- src/components/Forms/Forms.jsx
+++ src/components/Forms/Forms.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Forms.css'
+import { useTelegram } from '../hooks/useTg'
 
 const Forms = () => {
   const [country, setCountry] = useState('')
   const [street, setStreet] = useState('')
   const [subject, setSubject] = useState('')
+  const { tg } = useTelegram()
+
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: 'Send text',
+    })
+  }, [])
+
+  useEffect(() => {
+    if (!street || !country) {
+      tg.MainButton.hide()
+    } else {
+      tg.MainButton.show()
+    }
+  }, [country, street])
+
   const onChangeCountry = (el) => {
     setCountry(el.target.value)
   }
