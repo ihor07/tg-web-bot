63e22665f3631e8cd88cb5a8e3d3ccb9f09358d0
Ihor <ngorekk@gmail.com>
2022-12-12 14:36:34 +0100

    useState on forms

--- src/components/Forms/Forms.jsx
+++ src/components/Forms/Forms.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Forms.css'
 
 const Forms = () => {
+  const [country, setCountry] = useState('')
+  const [street, setStreet] = useState('')
+  const [subject, setSubject] = useState('')
+  const onChangeCountry = (el) => {
+    setCountry(el.target.value)
+  }
+  const onChangeStreet = (el) => {
+    setStreet(el.target.value)
+  }
+  const onChangeSubject = (el) => {
+    setSubject(el.target.value)
+  }
   return (
     <div className={'forms'}>
       <h3>Input your info</h3>
-      <input className={'input'} type="text" placeholder={'Country'} />
-      <input className={'input'} type="text" placeholder={'Street'} />
-      <select className={'select'}>
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Country'}
+        value={country}
+        onChange={onChangeCountry}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Street'}
+        value={street}
+        onChange={onChangeStreet}
+      />
+      <select value={subject} onChange={onChangeStreet} className={'select'}>
         <option value={'physic'}>physical</option>
         <option value={'legal'}>legal</option>
       </select>
