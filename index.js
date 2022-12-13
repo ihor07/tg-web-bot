27686e2d70ecc1fc047e226ad026daff48571910
Ihor <ngorekk@gmail.com>
2022-12-13 13:11:53 +0100

    not_final

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api')
+const express = require('express')
+const cors = require('cors')
 
 const token = '5823526718:AAESdvdx3ik1qMlRuv21Q5NWW1easW0BrRc'
 
 const webAppUrl = 'https://teal-unicorn-943661.netlify.app'
 
 const bot = new TelegramBot(token, { polling: true })
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id
@@ -43,3 +50,30 @@ bot.on('message', async (msg) => {
   }
   //  bot.sendMessage(chatId, 'Received your message')
 })
+
+app.post('/web-data', async (req, res) => {
+  const { queryId, products, totalPrice } = req.body
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: 'Success buy',
+      input_message_content: {
+        message_text:
+          'Сongratulations on your purchase for the amount ' + totalPrice,
+      },
+    })
+    return res.status(200).json({})
+  } catch (error) {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: "Don't success buy :(",
+      input_message_content: { message_text: "Don't success buy :(" },
+    })
+    return res.status(500).json({})
+  }
+})
+
+const PORT = 8000
+app.listen(PORT, () => console.log('server started at port ' + PORT))
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useTelegram } from '../hooks/useTg'
+import { useCallback, useEffect } from 'react'
 import ProductItem from '../ProductItem/ProductItem'
 import './ProductList.css'
 
@@ -23,7 +24,29 @@ const getTotalPrice = (items) => {
 const ProductList = () => {
   const [addedItems, setAddedItems] = useState([])
 
-  const { tg } = useTelegram()
+  const { tg, queryId } = useTelegram()
+
+  const onSendData = useCallback(() => {
+    const data = {
+      products: addedItems,
+      totalPrice: getTotalPrice(addedItems),
+      queryId,
+    }
+    fetch('http://localhost:8000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+  }, [])
+
+  useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData)
+    return () => {
+      tg.offEvent('mainButtonClicked', onSendData)
+    }
+  }, [onSendData])
 
   const onAdd = (product) => {
     const alreadyAdded = addedItems.find((item) => item.id === product.id)
